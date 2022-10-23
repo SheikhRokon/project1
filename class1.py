71901cae20e4f239d0b9179f32982ec9def0e16e
SheikhRokon <mdrokon.u545@gmail.com>
2022-10-23 09:13:07 +0600

    first commit

---
+++ class1.py
@@ -0,0 +1,54 @@
+# x=20
+# y=10
+# print(x+y)
+
+
+# print(x-y)
+
+
+# x1=200
+# y1=10
+
+# print(x1*y1)
+
+# a=30
+# b=6
+
+# print(a//b)
+
+# c=16
+# d=29
+
+
+# print(c**d)
+
+
+
+
+
+
+# q=124
+# print(q)
+# print(type(q))
+
+# x=str(q)
+# print(type(x))
+
+
+
+
+
+# a=15
+
+# a *= 15
+# a = a * 15
+
+# print(a)
+
+
+z = 43 
+ 
+# z -=43
+
+z=z-43
+print(z)
---
+++ class2.py
@@ -0,0 +1,50 @@
+# a=5 
+# b=6
+
+
+# if a == b: 
+#     print("ok")
+# elif a ==b:
+#     print ("ok 2")
+
+# else:
+#     print("not ok")
+
+# bangla = 32
+
+# if bangla >=36   
+#    print("pass")
+
+# else: 
+#     # print("fail")
+
+# math = 32
+
+# if math >= 80:
+#     print("A+")
+# elif math >= 70:
+#     print("A")
+# else:
+#     print("fail")
+
+bangla= 33
+
+# if bangla >100 and banngla < 1:
+#     print ("invaild number")
+bangla = 65
+
+# if bangla <100 or bangla >23:
+#     print ("condition ok")
+
+# for x in range(100):  
+#     print(x)
+
+# for x in range (1,999,17):
+#   print(x)
+a = int(input("enter your start num:"))
+b = int(input("enter your last num:"))
+sum= 0 
+
+for arif in range (a,b):
+    sum = sum + arif
+print(sum)
